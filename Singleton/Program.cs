f43de893cda1d5d479257310d4ec4ff2babafbd5
MahmoudAdelJR <mahmoudadeljr14@gmail.com>
2023-01-30 12:41:12 +0200

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = Enumerable.Range(0, 15);
+            Parallel.ForEach(range, i =>
+            {
+                var voteMachine = VoteMachine.GetVoteMachine;
+                voteMachine.RegisterVote();
+            });
+            Console.WriteLine("Total Votes is --> "+VoteMachine.GetVoteMachine.TotalVotes);
+        }
+    }
+}
+/// <Credits>
+/// credits reserved for https://www.tutorialsteacher.com/csharp/singleton
+/// <Credits>
---
+++ Singleton/VoteMachine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class VoteMachine
+    {
+        private static VoteMachine _voteMachine = null;
+        private int _totalVotes = 0;
+        private static readonly object locker = new object();
+        private VoteMachine()
+        {
+
+        }
+        public static VoteMachine GetVoteMachine
+        {
+            get
+             { 
+                    lock(locker)
+                    {
+                        if (_voteMachine is null)
+                        {
+                            _voteMachine = new VoteMachine();
+                        }
+                    }
+                return _voteMachine;
+            }
+        }
+
+        public void RegisterVote()
+        {
+            _totalVotes += 1;
+            Console.WriteLine("Registered Vote !");
+        }
+
+        public int TotalVotes
+        {
+            get
+            {
+                return _totalVotes;
+            }
+        }
+
+    }
+}
